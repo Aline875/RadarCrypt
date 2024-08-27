675a2b1461b27be1fde40865edb9e6e07ba44862
Aline <113183673+Aline875@users.noreply.github.com>
2024-08-27 09:55:03 -0300

    nav-pt-1

--- radarcrypt/tailwind.config.ts
+++ radarcrypt/tailwind.config.ts
@@ -13,6 +13,12 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+
+      colors:
+      {
+        'bg-custom-light':'#f1f5ff',
+        'text-font-color':'#171717'
+      }
     },
   },
   plugins: [],
